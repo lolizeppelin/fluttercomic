d233d0404df9d2e2047487d494b5086edbfb9e62
lolizeppelin <lolizeppelin@gmail.com>
2018-09-05 15:03:38 +0800

    api接口修改

--- fluttercomic/api/client/__init__.py
+++ fluttercomic/api/client/__init__.py
@@ -20,10 +20,14 @@ class FlutterComicClient(GopHttpClientApi):
     PRIVATE = 'private'
     PUBLIC = 'public'
 
+    PRIVATEVERESION = 'n1.0'
+
     # -----------users api-----------------
     def users_index(self, token, body=None):
-        headers = {common.TOKENNAME: token, common.FERNETHEAD: True}
-        resp, results = self.get(action=self.users_path % self.PRIVATE, headers=headers, body=body)
+        headers = {common.TOKENNAME: token, common.FERNETHEAD: 'yes'}
+        print headers
+        resp, results = self.get(action=self.users_path % self.PRIVATE, headers=headers, body=body,
+                                 version=self.PRIVATEVERESION)
         if results['resultcode'] != common.RESULT_SUCCESS:
             raise ServerExecuteRequestError(message='list fluttercomic users fail:%d' % results['resultcode'],
                                             code=resp.status_code,
@@ -40,8 +44,9 @@ class FlutterComicClient(GopHttpClientApi):
         return results
 
     def user_show(self, uid, token, body=None):
-        headers = {common.TOKENNAME: token, common.FERNETHEAD: True}
-        resp, results = self.get(action=self.user_path % (self.PRIVATE, uid), headers=headers, body=body)
+        headers = {common.TOKENNAME: token, common.FERNETHEAD: 'yes'}
+        resp, results = self.get(action=self.user_path % (self.PRIVATE, uid), headers=headers, body=body,
+                                 version=self.PRIVATEVERESION)
         if results['resultcode'] != common.RESULT_SUCCESS:
             raise ServerExecuteRequestError(message='show fluttercomic user fail:%d' % results['resultcode'],
                                             code=resp.status_code,
@@ -49,8 +54,9 @@ class FlutterComicClient(GopHttpClientApi):
         return results
 
     def user_update(self, uid, token, body=None):
-        headers = {common.TOKENNAME: token, common.FERNETHEAD: True}
-        resp, results = self.put(action=self.user_path % (self.PRIVATE, uid), headers=headers, body=body)
+        headers = {common.TOKENNAME: token, common.FERNETHEAD: 'yes'}
+        resp, results = self.put(action=self.user_path % (self.PRIVATE, uid), headers=headers, body=body,
+                                 version=self.PRIVATEVERESION)
         if results['resultcode'] != common.RESULT_SUCCESS:
             raise ServerExecuteRequestError(message='update fluttercomic user fail:%d' % results['resultcode'],
                                             code=resp.status_code,
@@ -58,8 +64,9 @@ class FlutterComicClient(GopHttpClientApi):
         return results
 
     def user_delete(self, uid, token, body=None):
-        headers = {common.TOKENNAME: token, common.FERNETHEAD: True}
-        resp, results = self.delete(action=self.user_path % (self.PRIVATE, uid), headers=headers, body=body)
+        headers = {common.TOKENNAME: token, common.FERNETHEAD: 'yes'}
+        resp, results = self.delete(action=self.user_path % (self.PRIVATE, uid), headers=headers, body=body,
+                                    version=self.PRIVATEVERESION)
         if results['resultcode'] != common.RESULT_SUCCESS:
             raise ServerExecuteRequestError(message='delete fluttercomic user fail:%d' % results['resultcode'],
                                             code=resp.status_code,
@@ -67,9 +74,9 @@ class FlutterComicClient(GopHttpClientApi):
         return results
 
     def user_books(self, uid, token, body=None):
-        headers = {common.TOKENNAME: token, common.FERNETHEAD: True}
+        headers = {common.TOKENNAME: token, common.FERNETHEAD: 'yes'}
         resp, results = self.get(action=self.user_path_ex % (self.PRIVATE, uid, 'books'), headers=headers,
-                                 body=body)
+                                 body=body, version=self.PRIVATEVERESION)
         if results['resultcode'] != common.RESULT_SUCCESS:
             raise ServerExecuteRequestError(message='get fluttercomic user books fail:%d' % results['resultcode'],
                                             code=resp.status_code,
@@ -77,9 +84,9 @@ class FlutterComicClient(GopHttpClientApi):
         return results
 
     def user_order(self, uid, token, body=None):
-        headers = {common.TOKENNAME: token, common.FERNETHEAD: True}
+        headers = {common.TOKENNAME: token, common.FERNETHEAD: 'yes'}
         resp, results = self.put(action=self.user_path_ex % (self.PRIVATE, uid, 'order'), headers=headers,
-                                 body=body)
+                                 body=body, version=self.PRIVATEVERESION)
         if results['resultcode'] != common.RESULT_SUCCESS:
             raise ServerExecuteRequestError(message='fluttercomic user create order fail:%d' % results['resultcode'],
                                             code=resp.status_code,
@@ -104,9 +111,9 @@ class FlutterComicClient(GopHttpClientApi):
         return results
 
     def comics_create(self, token, body=None):
-        headers = {common.TOKENNAME: token, common.FERNETHEAD: True}
+        headers = {common.TOKENNAME: token, common.FERNETHEAD: 'yes'}
         resp, results = self.retryable_post(action=self.comics_path % self.PRIVATE, headers=headers,
-                                            body=body)
+                                            body=body, version=self.PRIVATEVERESION)
         if results['resultcode'] != common.RESULT_SUCCESS:
             raise ServerExecuteRequestError(message='create fluttercomic comic fail:%d' % results['resultcode'],
                                             code=resp.status_code,
@@ -122,9 +129,9 @@ class FlutterComicClient(GopHttpClientApi):
         return results
 
     def comic_show_private(self, cid, token, body=None):
-        headers = {common.TOKENNAME: token, common.FERNETHEAD: True}
+        headers = {common.TOKENNAME: token, common.FERNETHEAD: 'yes'}
         resp, results = self.get(action=self.comic_path % (self.PRIVATE, cid), headers=headers,
-                                 body=body)
+                                 body=body, version=self.PRIVATEVERESION)
         if results['resultcode'] != common.RESULT_SUCCESS:
             raise ServerExecuteRequestError(message='show fluttercomic comic fail:%d' % results['resultcode'],
                                             code=resp.status_code,
@@ -132,8 +139,9 @@ class FlutterComicClient(GopHttpClientApi):
         return results
 
     def comic_update(self, cid, token, body=None):
-        headers = {common.TOKENNAME: token, common.FERNETHEAD: True}
-        resp, results = self.put(action=self.comic_path % (self.PRIVATE, cid), headers=headers, body=body)
+        headers = {common.TOKENNAME: token, common.FERNETHEAD: 'yes'}
+        resp, results = self.put(action=self.comic_path % (self.PRIVATE, cid), headers=headers, body=body,
+                                 version=self.PRIVATEVERESION)
         if results['resultcode'] != common.RESULT_SUCCESS:
             raise ServerExecuteRequestError(message='update fluttercomic comic fail:%d' % results['resultcode'],
                                             code=resp.status_code,
@@ -141,8 +149,9 @@ class FlutterComicClient(GopHttpClientApi):
         return results
 
     def comic_delete(self, cid, token, body=None):
-        headers = {common.TOKENNAME: token, common.FERNETHEAD: True}
-        resp, results = self.delete(action=self.comic_path % (self.PRIVATE, cid), headers=headers, body=body)
+        headers = {common.TOKENNAME: token, common.FERNETHEAD: 'yes'}
+        resp, results = self.delete(action=self.comic_path % (self.PRIVATE, cid), headers=headers, body=body,
+                                    version=self.PRIVATEVERESION)
         if results['resultcode'] != common.RESULT_SUCCESS:
             raise ServerExecuteRequestError(message='update fluttercomic comic fail:%d' % results['resultcode'],
                                             code=resp.status_code,
@@ -150,9 +159,9 @@ class FlutterComicClient(GopHttpClientApi):
         return results
 
     def comic_mark(self, cid, uid, token, body):
-        headers = {common.TOKENNAME: token, common.FERNETHEAD: True}
+        headers = {common.TOKENNAME: token, common.FERNETHEAD: 'yes'}
         resp, results = self.put(action=self.mark_path % (self.PRIVATE, cid, uid),
-                                 headers=headers, body=body)
+                                 headers=headers, body=body, version=self.PRIVATEVERESION)
         if results['resultcode'] != common.RESULT_SUCCESS:
             raise ServerExecuteRequestError(message='mark fluttercomic comic chapter fail:%d' % results['resultcode'],
                                             code=resp.status_code,
@@ -160,9 +169,9 @@ class FlutterComicClient(GopHttpClientApi):
         return results
 
     def comic_unmark(self, cid, uid, token, body):
-        headers = {common.TOKENNAME: token, common.FERNETHEAD: True}
+        headers = {common.TOKENNAME: token, common.FERNETHEAD: 'yes'}
         resp, results = self.delete(action=self.mark_path % (self.PRIVATE, cid, uid),
-                                    headers=headers, body=body)
+                                    headers=headers, body=body, version=self.PRIVATEVERESION)
         if results['resultcode'] != common.RESULT_SUCCESS:
             raise ServerExecuteRequestError(message='unmark fluttercomic comic fail:%d' % results['resultcode'],
                                             code=resp.status_code,
@@ -171,9 +180,9 @@ class FlutterComicClient(GopHttpClientApi):
 
     # ----------chapter api ---------------
     def chapter_buy(self, cid, chapter, uid, token, body):
-        headers = {common.TOKENNAME: token, common.FERNETHEAD: True}
+        headers = {common.TOKENNAME: token, common.FERNETHEAD: 'yes'}
         resp, results = self.post(action=self.buy_path % (self.PRIVATE, cid, chapter, uid),
-                                  headers=headers, body=body)
+                                  headers=headers, body=body, version=self.PRIVATEVERESION)
         if results['resultcode'] != common.RESULT_SUCCESS:
             raise ServerExecuteRequestError(message='buy fluttercomic comic chapter fail:%d' % results['resultcode'],
                                             code=resp.status_code,
@@ -181,9 +190,9 @@ class FlutterComicClient(GopHttpClientApi):
         return results
 
     def chapter_create(self, cid, chapter, token, body):
-        headers = {common.TOKENNAME: token, common.FERNETHEAD: True}
+        headers = {common.TOKENNAME: token, common.FERNETHEAD: 'yes'}
         resp, results = self.retryable_post(action=self.chapter_path % (self.PRIVATE, cid, chapter),
-                                            headers=headers, body=body)
+                                            headers=headers, body=body, version=self.PRIVATEVERESION)
         if results['resultcode'] != common.RESULT_SUCCESS:
             raise ServerExecuteRequestError(message='fluttercomic comic create chapter fail:%d' %
                                                     results['resultcode'],
@@ -192,9 +201,9 @@ class FlutterComicClient(GopHttpClientApi):
         return results
 
     def chapter_finsh(self, cid, chapter, token, body):
-        headers = {common.TOKENNAME: token, common.FERNETHEAD: True}
+        headers = {common.TOKENNAME: token, common.FERNETHEAD: 'yes'}
         resp, results = self.patch(action=self.chapter_path % (self.PRIVATE, cid, chapter),
-                                   headers=headers, body=body)
+                                   headers=headers, body=body, version=self.PRIVATEVERESION)
         if results['resultcode'] != common.RESULT_SUCCESS:
             raise ServerExecuteRequestError(message='fluttercomic comic create finish fail:%d' %
                                                     results['resultcode'],
@@ -203,9 +212,9 @@ class FlutterComicClient(GopHttpClientApi):
         return results
 
     def chapter_unfinsh(self, cid, chapter, token, body):
-        headers = {common.TOKENNAME: token, common.FERNETHEAD: True}
+        headers = {common.TOKENNAME: token, common.FERNETHEAD: 'yes'}
         resp, results = self.delete(action=self.chapter_path % (self.PRIVATE, cid, chapter),
-                                    headers=headers, body=body)
+                                    headers=headers, body=body, version=self.PRIVATEVERESION)
         if results['resultcode'] != common.RESULT_SUCCESS:
             raise ServerExecuteRequestError(message='fluttercomic comic create unfinish fail:%d' %
                                                     results['resultcode'],
