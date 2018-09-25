2187b5cf3af9c931693842fa042e331ecc14529a
lolizeppelin <lolizeppelin@gmail.com>
2018-09-25 23:15:10 +0800

    默认日志等级

--- fluttercomic/plugin/convert/__init__.py
+++ fluttercomic/plugin/convert/__init__.py
@@ -11,7 +11,7 @@ CONVERT = systemutils.find_executable('fluttercomic-resize')
 def convert_cover(target, rename='main.jpg', size='1200x900', maxsize=250000, logfile=None):
     args = [CONVERT, '--target', target, '-s', size, '-m', str(maxsize), '-r', rename, '-o', '15']
     if logfile:
-        args.extend(['--log-file', logfile])
+        args.extend(['--log-file', logfile, '--loglevel', 'info'])
     sub = subprocess.Popen(args, close_fds=True, executable=CONVERT)
     systemutils.subwait(sub)
 
@@ -20,6 +20,6 @@ def convert_chapter(src, dst, key, size='800x600', maxsize=250000, logfile=None)
     # call convert
     args = [CONVERT, '--target', dst, '-s', size, '-m', str(maxsize), '-k', key, '-o', '3600']
     if logfile:
-        args.extend(['--log-file', logfile])
+        args.extend(['--log-file', logfile, '--loglevel', 'info'])
     sub = subprocess.Popen(args, close_fds=True, executable=CONVERT)
     systemutils.subwait(sub)
