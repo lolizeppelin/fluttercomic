abaa89b3bb9a89fe71f3d41d3a6b864200837501
lolizeppelin <lolizeppelin@gmail.com>
2018-10-19 14:30:46 +0800

    小bug修复

--- fluttercomic/plugin/convert/__init__.py
+++ fluttercomic/plugin/convert/__init__.py
@@ -13,17 +13,17 @@ def convert_cover(target, rename='main.webp', size='1600x1200', maxsize=250000, 
     if logfile:
         args.extend(['--log-file', logfile, '--loglevel', 'info'])
         with open(logfile, 'w') as f:
-            f.write('%s' % ' '.join(args))
+            f.write('%s\n' % ' '.join(args))
     sub = subprocess.Popen(args, close_fds=True, executable=CONVERT)
     systemutils.subwait(sub)
 
 
 def convert_chapter(dst, key, ext='webp', size='1200x900', maxsize=250000, logfile=None):
     # call convert
-    args = [CONVERT, '--target', dst, '-s',  '-e', ext, size, '-m', str(maxsize), '-k', key, '-o', '3600']
+    args = [CONVERT, '--target', dst, '-e', ext, '-s', size, '-m', str(maxsize), '-k', key, '-o', '3600']
     if logfile:
         args.extend(['--log-file', logfile, '--loglevel', 'info'])
         with open(logfile, 'w') as f:
-            f.write('%s' % ' '.join(args))
+            f.write('%s\n' % ' '.join(args))
     sub = subprocess.Popen(args, close_fds=True, executable=CONVERT)
     systemutils.subwait(sub)
